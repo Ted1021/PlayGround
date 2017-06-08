bd0c605adc3ff18d4f99ba68babbcd2d51dfc6c1
Ted <kimtx2017@gmail.com>
2017-06-08 10:50:16 +0900

    # Create Project
    - Practice 'TextView'

---
+++ BoostCamp/TextView/app/src/main/java/com/study/tedkim/textview/MainActivity.java
@@ -0,0 +1,26 @@
+package com.study.tedkim.textview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvBasic;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+
+    }
+
+    public void initView(){
+
+        tvBasic = (TextView) findViewById(R.id.textView_basic);
+        
+
+    }
+}
